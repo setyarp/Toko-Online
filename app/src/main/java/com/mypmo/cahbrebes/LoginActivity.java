2ab07aaccfb35d2e48703dacccc34ed80ab44c29
unknown <candramahardika06@gmail.com>
2023-06-06 12:13:57 +0700

    update login regis new

--- app/src/main/java/com/mypmo/cahbrebes/LoginActivity.java
+++ app/src/main/java/com/mypmo/cahbrebes/LoginActivity.java
@@ -1,14 +1,108 @@
 package com.mypmo.cahbrebes;
 
+import static android.util.Patterns.EMAIL_ADDRESS;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    TextView tvLupaPass, tvLogin;
+    EditText etEmailLog, etPasswordLog;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        // Mengecek jika user udah ada dan email udah verified maka langsung ke halaman main
+        if (user != null && user.isEmailVerified()) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
+        // Inisialisasi ID
+        etEmailLog = findViewById(R.id.emailLog);
+        etPasswordLog = findViewById(R.id.passwordLog);
+        tvLupaPass = findViewById(R.id.LupaPass);
+        tvLogin = findViewById(R.id.tvLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+
+//
+//        progress = new ProgressDialog(this);
+//        progress.setMessage("Loading data ...");
+//        progress.setCancelable(false);
+
+// Ketika button login di klik
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        // ketika button register di klik
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+    }
+
+    // proses login
+    private void login() {
+        String email = etEmailLog.getText().toString();
+        String password = etPasswordLog.getText().toString();
+
+        if (email.isEmpty()) {
+            etEmailLog.setError("Email tidak boleh kosong.");
+            etEmailLog.requestFocus();
+        } else if (!EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmailLog.setError("Email tidak valid.");
+            etEmailLog.requestFocus();
+        } else if (password.isEmpty()) {
+            etPasswordLog.setError("Password tidak boleh kosong.");
+            etPasswordLog.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "Login Succes", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                    }
+                    else if (!task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "Tidak Dapat Masuk, Silahkan Coba Lagi"
+                                , Toast.LENGTH_SHORT).show();
+//                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/mypmo/cahbrebes/RegisterActivity.java
+++ app/src/main/java/com/mypmo/cahbrebes/RegisterActivity.java
@@ -1,14 +1,87 @@
 package com.mypmo.cahbrebes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText nama, emailReg, passwordReg;
+    Button registerReg;
+    TextView tvLogin;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        getSupportActionBar().hide();
+
+        emailReg = findViewById(R.id.emailRegis);
+        nama = findViewById(R.id.fullname);
+        passwordReg = findViewById(R.id.passwordRegis);
+        registerReg = findViewById(R.id.btnReg);
+        tvLogin = findViewById(R.id.tvRegister);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser();
+//                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+
+    private void createUser() {
+        String email = emailReg.getText().toString();
+        String password = passwordReg.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            emailReg.setError("Email Cannot be Empty");
+            emailReg.requestFocus();
+        }
+        else if (TextUtils.isEmpty(password)){
+            passwordReg.setError("Password Cannot be Empty");
+            passwordReg.requestFocus();
+        }
+        else  {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "User Berhasil Registrasi", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }
+                    else if (!task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "User Gagal Registrasi", Toast.LENGTH_SHORT).show();
+//                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+            });
+        }
     }
 }
