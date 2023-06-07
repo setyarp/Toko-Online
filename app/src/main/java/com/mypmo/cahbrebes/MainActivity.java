6efdd7e48a8031dce4147ecf9e936ff8be4bf61a
setyarp <setyarizkypradana@gmail.com>
2023-06-07 12:39:53 +0700

    logout session and add layout main

--- app/src/main/java/com/mypmo/cahbrebes/MainActivity.java
+++ app/src/main/java/com/mypmo/cahbrebes/MainActivity.java
@@ -2,13 +2,38 @@ package com.mypmo.cahbrebes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Deklarasi Variable
+    private TextView logout;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Deklarasi ID
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(MainActivity.this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
