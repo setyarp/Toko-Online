c6731aff8ddc3544f63bb9e81e663307f05d3934
setyarp <setyarizkypradana@gmail.com>
2023-06-07 09:47:50 +0700

    fix crash & add feat email verification

--- app/src/main/java/com/mypmo/cahbrebes/LoginActivity.java
+++ app/src/main/java/com/mypmo/cahbrebes/LoginActivity.java
@@ -5,7 +5,9 @@ import static android.util.Patterns.EMAIL_ADDRESS;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -92,9 +94,7 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        Toast.makeText(getApplicationContext(), "Login Succes", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-
+                        checkIfEmailVerified();
                     }
                     else if (!task.isSuccessful()){
                         Toast.makeText(getApplicationContext(), "Tidak Dapat Masuk, Silahkan Coba Lagi"
@@ -104,5 +104,68 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    // verifikasi email
+    private void checkIfEmailVerified()
+    {
+        user = mAuth.getCurrentUser();
+
+        if (user.isEmailVerified())
+        {
+            Toast.makeText(LoginActivity.this, "Berhasil login", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+        else
+        {
+            showDialog();
+        }
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        alertDialogBuilder.setTitle("Email kamu belum diverifikasi!");
+
+        alertDialogBuilder
+                .setMessage("Kirim ulang email verifikasi?")
+                .setCancelable(false)
+                .setPositiveButton("Ya",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        sendVerificationEmail();
+                    }
+                })
+                .setNegativeButton("Tidak",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+    }
+
+    private void sendVerificationEmail() {
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Email verifikasi berhasil dikirim!", Toast.LENGTH_SHORT).show();
+                            mAuth.signOut();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Email verifikasi tidak dapat dikirim!", Toast.LENGTH_SHORT).show();
+                            overridePendingTransition(0, 0);
+                            finish();
+                            overridePendingTransition(0, 0);
+                            startActivity(getIntent());
+
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/mypmo/cahbrebes/RegisterActivity.java
+++ app/src/main/java/com/mypmo/cahbrebes/RegisterActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -73,6 +74,7 @@ public class RegisterActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
+                        sendVerificationEmail();
                         Toast.makeText(RegisterActivity.this, "User Berhasil Registrasi", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                     }
@@ -83,5 +85,26 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void sendVerificationEmail() {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Email verifikasi berhasil dikirim!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "Email verifikasi tidak dapat dikirim!", Toast.LENGTH_SHORT).show();
+                            overridePendingTransition(0, 0);
+                            finish();
+                            overridePendingTransition(0,  0);
+                            startActivity(getIntent());
+                        }
+                    }
+                });
     }
 }
